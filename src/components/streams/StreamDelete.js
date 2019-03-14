112732e685d7cd55c5d7a7158f76b2a095d19810
Kevin Yeh <revinyeh@gmail.com>
2019-03-14 16:29:33 -0700

    DeleteStream component with all funtion done

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import Modal from '../Modal';
 import history from '../../history';
 import { fetchStream, deleteStream } from '../../actions'
@@ -10,10 +11,11 @@ class StreamDelete extends React.Component {
   }
 
   renderActions() {
+    const { id } = this.props.match.params;
     return (
       <React.Fragment>
-        <button className="ui button negative">Delete</button>
-        <button className="ui button ">Cancel</button>
+        <button onClick={() => this.props.deleteStream(id)} className="ui button negative">Delete</button>
+        <Link to="/" className="ui button ">Cancel</Link>
       </React.Fragment>
     );
   }
@@ -27,15 +29,12 @@ class StreamDelete extends React.Component {
 
   render() {
     return (
-      <div>
-        StreamDelete
         <Modal
           title="Delete Stream"
           content={this.renderContent()}
           actions={this.renderActions()}
           onDismiss={() => history.push("/")}
         />
-      </div>
     )
   }
 }
